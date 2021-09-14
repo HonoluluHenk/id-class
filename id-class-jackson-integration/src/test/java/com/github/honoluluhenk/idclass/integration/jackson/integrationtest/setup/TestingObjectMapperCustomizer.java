4100cc7c6b9559c13eff43fe8dca9cacf8fd33f6
Christoph Linder <post@christoph-linder.ch>
2021-09-14 02:38:23 +0200

    delete unnecessary test setup

--- id-class-jackson-integration/src/test/java/com/github/honoluluhenk/idclass/integration/jackson/integrationtest/setup/TestingObjectMapperCustomizer.java
+++ id-class-jackson-integration/src/test/java/com/github/honoluluhenk/idclass/integration/jackson/integrationtest/setup/TestingObjectMapperCustomizer.java
@@ -2,9 +2,7 @@ package com.github.honoluluhenk.idclass.integration.jackson.integrationtest.setu
 
 import javax.inject.Singleton;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import com.github.honoluluhenk.idclass.integration.jackson.IDTypeModule;
 import io.quarkus.jackson.ObjectMapperCustomizer;
 
@@ -14,11 +12,6 @@ public class TestingObjectMapperCustomizer implements ObjectMapperCustomizer {
 
 	@Override
 	public void customize(ObjectMapper objectMapper) {
-		// To suppress serializing properties with null values
-		objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-		// Take property name from CTOR args' name
-		objectMapper.registerModule(new ParameterNamesModule());
-
 		objectMapper.registerModule(new IDTypeModule());
 	}
 
